c6748746e66d877bc95460bdf04e5e0607cf8785
Victor Lemesre <victor.lemesre@gmail.com>
2023-05-31 11:58:51 +0200

    end step6

--- TP5/HelloSpring/src/com/TP5/MainApp.java
+++ TP5/HelloSpring/src/com/TP5/MainApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld obj = (HelloWorld) context.getBean("helloWotld");
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 		obj.getMessage();
 	}
 
