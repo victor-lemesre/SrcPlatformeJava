b1ca4f091132600b006313b16589381ef8749915
11kainani <95407428+11kainani@users.noreply.github.com>
2023-05-31 11:31:54 +0200

    HelloWorld 1

--- TP5/HelloSpring/src/com/TP5/MainApp.java
+++ TP5/HelloSpring/src/com/TP5/MainApp.java
@@ -1,9 +1,14 @@
 package com.TP5;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
 
     }
 
